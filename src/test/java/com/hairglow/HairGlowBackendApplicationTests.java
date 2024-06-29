772f9696c733f29271295e2c40b2fb0443079b67
Fatimata <dramefatim75@gmail.com>
2024-06-29 18:27:26 +0200

    first commit

---
+++ src/test/java/com/hairglow/HairGlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hairglow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairGlowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
